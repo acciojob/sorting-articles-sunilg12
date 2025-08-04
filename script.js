709499de139939a240dae7e0c65274bbb287278d
Sunil Reddy Gaddam <gaddamsunilreddy20@gmail.com>
2025-08-04 13:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 
+			   'Pierce the Veil', 'Norma Jean', 'The Bled', 
+			   'Say Anything', 'The Midway State', 'We Came as Romans', 
+			   'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 
+			   'Anywhere But Here', 'An Old Dog'];
+
+function stripPrefix(name){
+	return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+const sorteBands = bands.sort((a,b) => {
+	const nameA = stripPrefix(a).toLowerCase();
+	const nameB = stripPrefix(b).toLowerCase();
+
+	return nameA.localeCompare(nameB);
+})
+
+const ul = document.getElementById("band");
+ul.innerHTML = sorteBands.map(bands => `<li>${bands}</li>`).join('');
+
+
